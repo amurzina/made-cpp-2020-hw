2ca98c1ff7a1d741124e8c7bba961eef77cc5df3
sasha <murzina93@gmail.com>
2020-09-22 02:38:32 +0700

    add solution

--- classify double/solution.c
+++ classify double/solution.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <stdlib.h>
 
+// constants from wiki
+#define PLUS_ZERO   0x0000000000000000
+#define MINUS_ZERO  0x8000000000000000
+#define PLUS_INF    0x7FF0000000000000
+#define MINUS_INF   0xFFF0000000000000
 
+#define SIGN        63
+#define FRCT_STRT   51
+
+#define EXP_MSK     0x7ff0000000000000
+#define FRCT_MSK    0x000fffffffffffff
+
+#define POSITIVE    0
+#define NEGATIVE    1
 
 /**
  * Library-level functions.
@@ -15,52 +27,60 @@ uint64_t convertToUint64 (double number) {
 }
 
 bool getBit (const uint64_t number, const uint8_t index) {
-    /// Your code here...
+    uint64_t mask = 1ull << index;
+    return number & mask;
 }
 
+bool isNormal (const uint64_t number) {
+    return ((number & EXP_MSK) != 0) && ((number & FRCT_MSK) != 0);
+}
+
+bool isDenormal(const uint64_t number) {
+    return ((number & EXP_MSK) == 0) && getBit(number, 0);
+}
 
 /**
  * Checkers here:
  */
 
 bool checkForPlusZero (uint64_t number) {
-    /// Your code here.
+    return number == PLUS_ZERO;
 }
 
 bool checkForMinusZero (uint64_t number) {
-    return number == 0x8000000000000000;
+    return number == MINUS_ZERO;
 }
 
 bool checkForPlusInf (uint64_t number) {
-    /// Your code here.
+    return number == PLUS_INF;
 }
 
 bool checkForMinusInf (uint64_t number) {
-    /// Your code here.
+    return number == MINUS_INF;
 }
 
 bool checkForPlusNormal (uint64_t number) {
-    /// Your code here.
+    return getBit(number, SIGN) == POSITIVE && isNormal(number);
 }
 
 bool checkForMinusNormal (uint64_t number) {
-    /// Your code here.
+    return getBit(number, SIGN) == NEGATIVE && isNormal(number);
 }
 
 bool checkForPlusDenormal (uint64_t number) {
-    /// Your code here.
+return getBit(number, SIGN) == POSITIVE && isDenormal(number);
 }
 
 bool checkForMinusDenormal (uint64_t number) {
-    /// Your code here.
+return getBit(number, SIGN) == NEGATIVE && isDenormal(number);
 }
 
 bool checkForSignalingNan (uint64_t number) {
-    /// Your code here.
+    return (number & FRCT_MSK) == FRCT_MSK && (number & FRCT_MSK) == 1 && getBit(number, FRCT_STRT) == NEGATIVE;
 }
 
 bool checkForQuietNan (uint64_t number) {
-    /// Your code here.
+    return (number & FRCT_MSK) == FRCT_MSK && getBit(number, FRCT_STRT);
 }
 
 
