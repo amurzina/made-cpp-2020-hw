180c35b44bee4d2c8a52680a1ddf574a80f8fce2
sasha <murzina93@gmail.com>
2020-10-04 09:42:43 +0300

    add solution

--- simple_compose/src/main.cpp
+++ simple_compose/src/main.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
 #include <functional>
 
-typedef std::function<int (int)> Op;
+typedef std::function<int(int)> Op;
 
 
-
-Op compose (size_t n, Op ops[]) {
-    /// Your code goes here.
+Op compose(size_t n, Op ops[]) {
+    return [n, ops](int x) {
+        for (int i = n - 1; i >= 0; i--) {
+            x = ops[i](x);
+            //std::cout << x << " " << i << std::endl;
+        }
+        return x;
+    };
 }
 
 
-int main () {
+int main() {
     /// Simple tests:
 
     Op op1 =
-        [] (int x) -> int {
-            return x + 1;
-        };
+            [](int x) -> int {
+                return x + 1;
+            };
 
     auto op2 =
-        [] (int p) -> Op {
-            return [p] (int x) {
-                return p * x;
+            [](int p) -> Op {
+                return [p](int x) {
+                    return p * x;
+                };
             };
-        };
 
-    {
+    {   //     11 (10+1) <- 10 (5*2) <- 5 (4+1) <- 4 (2*2)
         Op ops[4] = {op1, op2(2), op1, op2(2)};
         if (compose(4, ops)(2) != 11) {
             std::cout << "FAILED AT TEST 1" << std::endl;
@@ -33,7 +38,7 @@ int main () {
         }
     }
 
-    {
+    {   //            55 <- 54(27*2) <-27 <-26 <-25 <-24(12*2) <-12(6*2) <-6(5+1)
         Op ops[8] = {op1, op2(2), op1, op1, op1, op2(2), op2(2), op1};
         if (compose(8, ops)(5) != 55) {
             std::cout << "FAILED AT TEST 2" << std::endl;
